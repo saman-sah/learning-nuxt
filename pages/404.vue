fb47d5b5b81492d531aa0cd256ece086c1aa201b
SamanSah <sahraei.saman91@gmail.com>
2023-07-08 22:03:51 +0330

    copeletion second project

---
+++ pages/404.vue
@@ -0,0 +1,17 @@
+<script setup>
+definePageMeta({
+    layout: "restaurants",
+})
+</script>
+
+<template>
+    <div class="container">
+        <h1>Page not found</h1>
+    </div>
+  
+</template>
+
+<style>
+
+</style>
+
