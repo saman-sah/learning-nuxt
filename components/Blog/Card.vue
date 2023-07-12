36d344bf9e31b8945db7b5ac6d99e260cbf58edf
SamanSah <sahraei.saman91@gmail.com>
2023-07-12 10:31:56 +0330

    composables

---
+++ components/Blog/Card.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import useDarkMode from '../../composables/useDarkMode';
+
+interface CardProps {
+  card: {
+    img: string;
+    title: string;
+    author: string;
+  };
+}
+const { isDarkMode } = useDarkMode();
+const props = defineProps<CardProps>();
+</script>
+
+<template>
+  <div
+    class="card"
+    :style="isDarkMode ? { backgroundColor: 'black', color: 'white' } : ''"
+  >
+    <img :src="props.card.img" alt="" />
+    <div class="content">
+      <h3>{{ props.card.title }}</h3>
+      <p>{{ props.card.author }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  box-shadow: 1px 1px 5px black;
+  background: white;
+  width: 48%;
+  margin-bottom: 30px;
+  border-radius: 5px;
+  overflow: hidden;
+}
+
+img {
+  width: 100%;
+  height: 200px;
+}
+
+.content {
+  padding: 10px;
+}
+
+p {
+  margin-top: 10px;
+}
+</style>
